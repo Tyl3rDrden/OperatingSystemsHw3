f3122c36943ed0eafb1ac49b4e234029b0e573fc
Tyl3rDrden <jonathan.ben@campus.technion.ac.il>
2024-03-09 09:31:15 +0000

    Ron  code exposotition

--- server.c
+++ server.c
@@ -5,32 +5,42 @@
 
 #define NUMBEROFWORKERTHREADS 4
 #define QUEUESIZE 100
-
+#define FALSE 0
+#define TRUE 1
+#define RETURNERROR -1
 
 pthread_mutex_t bufferMutex;
 
 //Coniditional Variables for the buffer
-pthread_cond_t is_empty; 
-pthread_cond_t is_full; 
+pthread_cond_t masterWakeUp; 
+pthread_cond_t workerWakeUp; 
+
+//Global variavles
 
 
 typedef struct requestsBuffer {
-    int requests[QUEUESIZE] = {0}; // Initialize all elements to 0
-    int head = 0;
-    int tail = 0;
-} requestsBuffer reqBuffer;
+    int requests[QUEUESIZE]; // Initialize all elements to 0
+    int head;
+    int tail;
+    int is_empty; // This is essentially a booleaen 
+    int is_full;
+} requestsBuffer;
+
+requestsBuffer reqbuffer;
 
 int getRequest()
 {
     if(reqbuffer.head == reqbuffer.tail)
     {
-        is_empty = false;
-        return -1;
+        reqbuffer.is_empty = TRUE;
+        return RETURNERROR;
     }
     else
     {
         int request = reqbuffer.requests[reqbuffer.tail];
         reqbuffer.tail = (reqbuffer.tail + 1) % QUEUESIZE; //Increment the tail to get a fifo extraction
+        reqbuffer.is_full = FALSE;
+        pthread_cond_signal(&masterWakeUp);
         return request;
     }
 }
@@ -38,14 +48,16 @@ int addRequest(int connfd)
 {
     if((reqbuffer.head + 1) % QUEUESIZE == reqbuffer.tail)
     {
-        is_full = true;
-        return -1;
+        reqbuffer.is_full = TRUE;
+        return RETURNERROR;
         //We are full Return error code
     }
     else
     {
-        reqbuffer.head = (reqbuffer.head + 1) % QUEUESIZE;
         reqbuffer.requests[reqbuffer.head] = connfd;
+        reqbuffer.head = (reqbuffer.head + 1) % QUEUESIZE;
+        reqbuffer.is_empty = FALSE;
+        pthread_cond_signal(&workerWakeUp);
         return 0; //Success
     }
 }
@@ -88,26 +100,24 @@ void handleRequest(int connfd)
 void workerSingleThreadRoutine()
 {
         pthread_mutex_lock(&bufferMutex);
-        while(is_empty)
+        while(reqbuffer.is_empty)
         {
-            pthread_cond_wait(&is_empty, &bufferMutex);
+            pthread_cond_wait(&workerWakeUp, &bufferMutex);
             //Check that the buffer is not empty and wait on the conditional variable
             //Ask for resource, get the request, release the resource and startthe route later
         }
         int request = getRequest();
-        is_full = false;
         pthread_mutex_unlock(&bufferMutex);
-        if(request != -1)
+        if(request != RETURNERROR)
         {
             handleRequest(request);
         }
 
 }
-void threadRoutine()
+void* threadRoutine()
 {
     while(1){
-            //Check that the buffer is not full and wait on the conditional variable
-            //Ask for resource, get the request, release the resource and startthe route later
+        //this part will probably encapsulate the second aprt
         workerSingleThreadRoutine();
     }
 }
@@ -123,6 +133,7 @@ int main(int argc, char *argv[])
     int listenfd, connfd, port, clientlen;
     struct sockaddr_in clientaddr;
 
+
     getargs(&port, argc, argv);
 
     //Initializing the mutex
@@ -137,7 +148,7 @@ int main(int argc, char *argv[])
 
     //initializing the condition variables
     
-    if(!pthread_cond_init(&is_empty, NULL))
+    if(pthread_cond_init(&masterWakeUp, NULL) != 0)
     {
         perror("Condiiton variable is empty not set");
         return -1; 
@@ -145,21 +156,25 @@ int main(int argc, char *argv[])
         //Error has occured
     }
 
-    if(!pthread_cond_init(&is_full, NULL))
+    if(pthread_cond_init(&workerWakeUp, NULL) != 0)
     {
         perror("Condiiton variable is full not set");
         return -1; 
         
         //Error has occured
     }
-
+    //Init of the global variable
+    reqbuffer.is_empty = TRUE;
+    reqbuffer.is_full = FALSE;
+    reqbuffer.tail = 0;
+    reqbuffer.head = 0;
 
     //Here we need to create a queue of requests and make it empty
     //Also we need to initialize a fixed number of worker threads
     int threadIds[NUMBEROFWORKERTHREADS];
     for (int i = 0; i < NUMBEROFWORKERTHREADS; i++)
     {
-        if(!pthread_create(threadIds + i, NULL, threadRoutine, NULL))
+        if(pthread_create(threadIds + i, NULL, threadRoutine, NULL) != 0)
         {
             perror("Routine error");
             return -1; 
@@ -174,16 +189,14 @@ int main(int argc, char *argv[])
         connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
         //Add the request to the queue
         pthread_mutex_lock(&bufferMutex);
-        while(is_full)
+        while(reqbuffer.is_full)
         {
-            pthread_cond_wait(&is_full, &bufferMutex);
+            pthread_cond_wait(&masterWakeUp, &bufferMutex);
             //Check that the buffer is not full and wait on the conditional variable
             //Ask for resource, get the request, release the resource and startthe route later
         }
         addRequest(connfd);
-        is_empty = false;
         pthread_mutex_unlock(&bufferMutex);
-        pthread_cond_signal(&is_empty);
     }
 
 }
